ad95f3c4313bdc1842cd7c7bda45226b5a56a07b
hanshavin <hansevangelista@gmail.com>
2014-06-07 02:04:19 -0500

    problem 3 PRO implementation

--- Problem 3.clj
+++ Problem 3.clj
@@ -4,13 +4,23 @@
 ; Code by Hanshavin (@devHanshavin)
 
 
-(def n (atom 600851475143))
-(def i (atom 2))
-(def value (atom i))
+(defn solve [n, i]
+	(if(< (Math/sqrt n) i)
+		n
+		(#(let[x (mod n i)]
+			(cond
+				(= x 0) (solve (long(/ n i)) i)
+				:else (solve n (inc i)))))))
 
-(while (<= @i (Math/sqrt @n))
-	(while (zero? (mod @n @i))
-		(swap! n (fn [x] (/ x @i)))
-		(swap! value (fn [x] @i)))
-	(swap! i inc))
-(if (= @n 1) @value @n)
+(solve 600851475143 2)
+
+; (def n (atom 600851475143))
+; (def i (atom 2))
+; (def value (atom i))
+
+; (while (<= @i (Math/sqrt @n))
+; 	(while (zero? (mod @n @i))
+; 		(swap! n (fn [x] (/ x @i)))
+; 		(swap! value (fn [x] @i)))
+; 	(swap! i inc))
+; (if (= @n 1) @value @n)
